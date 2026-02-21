5b521269f879e924f43a8c78ef009421b1b579c9
Dannadx <diazdanna494@gmail.com>
2026-02-20 21:27:24 -0600

    Add files via upload
    Aplicación de consola en C# que permita al usuario ingresar una cadena de texto . El sistema procesa cada carácter de dicha cadena, obtiene su valor numérico equivalente en la tabla ASCII y aplica un desplazamiento de 3 unidades.

---
+++ EJERCICIO2/EJERCICIO2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class CODIGOASCII
+{
+    static void Main()
+    {
+        Console.WriteLine("EJERCICIO 2 ");//LINEA PARA MOSTRAR MENSAJE EN PANTALLA
+        Console.Write("Ingrese el código a interpretar "); //solicitar el codigo
+        string codigo = Console.ReadLine();//almacenar codigo en variable y permitir que el usuario ingrese datos
+
+        //ciclo for para recorrer el codigo, siempre y cuando sea menor al ancho del codigo
+        for (int i =0; i< codigo.Length; i++) 
+        {
+            char caracter = codigo[i]; //variable para guardar cada valor del código
+            int cod = (int)caracter + 3; //convierte los caracteres a enteros para poder realizar la suma
+            Console.Write(cod + " ");//escribe el numero y le suma 3 
+        }
+        
+
+    }
+}
